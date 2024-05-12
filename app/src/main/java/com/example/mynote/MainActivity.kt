88674fdfa4304314f23ba427eb16bb6140f0a4e6
Sandaru Abeykoon <129868635+SandaruAbey@users.noreply.github.com>
2024-05-12 07:29:52 +0530

    Commit Lab4 Mad

---
+++ app/src/main/java/com/example/mynote/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.example.mynote
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.lifecycle.ViewModelProvider
+import com.example.mynote.database.NoteDatabase
+import com.example.mynote.repository.NoteRepository
+import com.example.mynote.viewmodel.NoteViewModel
+import com.example.mynote.viewmodel.NoteViewModelFactory
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var noteViewModel: NoteViewModel
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        setupViewModel()
+
+    }
+    private fun setupViewModel(){
+        val noteRepository = NoteRepository(NoteDatabase(this,))
+        val viewModelProviderFactory = NoteViewModelFactory(application, noteRepository)
+        noteViewModel = ViewModelProvider(this, viewModelProviderFactory)[NoteViewModel::class.java]
+    }
+}
